3692640aa344d584e9ed8a8d3ff1b13c6e2eb27f
Nicolas Philibert <phil@Nicolass-MacBook-Pro.local>
2024-04-15 14:14:41 +0300

    add maxSubArraySum

---
+++ array/maxSubArraySum.py
@@ -0,0 +1,18 @@
+def maxSubarraySum(arr, n) :
+    sum = float("-inf")
+    currSum = 0
+    for i in range(n):
+        currSum += arr[i]
+
+        if currSum > sum :
+             sum = currSum
+        
+        if currSum < 0:
+            currSum = 0
+            
+    if sum < 0:
+        sum = 0
+    return sum
+
+
+print(maxSubarraySum([1, 2, 7, -4, 3, 2, -10, 9, 1], 9))
